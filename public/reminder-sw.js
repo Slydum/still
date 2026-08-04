bf4e868c1a279b77e8bd25ef32c4429afbd44a6c
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-05 03:33:32 +0800

    Make Still installable as a PWA

--- public/reminder-sw.js
+++ public/reminder-sw.js
@@ -1,3 +1,51 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('still-shell-v1').then((cache) => cache.addAll([
+      '/',
+      '/manifest.webmanifest',
+      '/icons/icon-192.png',
+      '/icons/icon-512.png',
+    ])),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key.startsWith('still-shell-') && key !== 'still-shell-v1').map((key) => caches.delete(key)),
+    )),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open('still-shell-v1').then((cache) => cache.put('/', copy));
+          return response;
+        })
+        .catch(() => caches.match('/')),
+    );
+    return;
+  }
+
+  if (new URL(event.request.url).origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open('still-shell-v1').then((cache) => cache.put(event.request, copy));
+        return response;
+      })),
+    );
+  }
+});
+
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(
