933564ecea33368ad466f3e3b9f8382c2cae501b
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-05 18:21:50 +0800

    Load graphics uniformity styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import './theme/tokens.css';
 import './theme/global.css';
+import './theme/uniformity.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
