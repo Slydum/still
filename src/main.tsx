51566d98c812e7170efbfb8b7a6981b2a72c17e9
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-05 18:47:13 +0800

    Load iOS form focus fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './app/App';
 import './theme/tokens.css';
 import './theme/global.css';
 import './theme/uniformity.css';
+import './theme/ios-form-fix.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
