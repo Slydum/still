6d5bd59c2cbccdeacf17b1fb33684c4a8048c0e9
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-08 18:56:29 +0800

    v0.4 phase 2: load Work visual refinement

--- src/main.tsx
+++ src/main.tsx
@@ -30,6 +30,7 @@ import './theme/v031-mobile-polish.css';
 import './theme/v04-home.css';
 import './theme/v04-home-refinement.css';
 import './theme/v04-home-device-pass.css';
+import './theme/v04-work.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
