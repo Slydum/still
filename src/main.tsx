035d4bcbe46ed1f19303198d8210be923d9cdb19
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-25 21:10:32 +0800

    Mount redesigned mobile home

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import { APP_BASENAME } from './app/appLocation';
 import { PersistenceStatusBanner } from './components/PersistenceStatusBanner';
+import { MobileHomeOverlay } from './features/dashboard/MobileHomeOverlay';
 import './theme/runtimeTheme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter basename={APP_BASENAME}>
       <App />
+      <MobileHomeOverlay />
       <PersistenceStatusBanner />
     </BrowserRouter>
   </React.StrictMode>,
