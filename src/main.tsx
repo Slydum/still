4d558a16de441994193b5f2357f22f9f9841bb34
Slydum <76755508+Slydum@users.noreply.github.com>
2026-08-25 21:11:17 +0800

    Apply mobile home shell visibility

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './app/App';
 import { APP_BASENAME } from './app/appLocation';
 import { PersistenceStatusBanner } from './components/PersistenceStatusBanner';
 import { MobileHomeOverlay } from './features/dashboard/MobileHomeOverlay';
+import './features/dashboard/mobile-home-shell.css';
 import './theme/runtimeTheme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
